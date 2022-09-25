2b93b9915ee5b4c9ad9ddfb2e4e6d9da6bb3beee
stark-af1 <ahmedabdelfattah97@gmail.com>
2022-09-25 12:54:44 +0200

    home page handling

--- lib/features/hotels/presentation/screens/home_screen/home_screen.dart
+++ lib/features/hotels/presentation/screens/home_screen/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
+      //handling
       body: CustomScrollView(
         slivers: [
           SliverPersistentHeader(
